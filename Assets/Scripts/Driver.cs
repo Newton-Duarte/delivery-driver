62ec1e987b59103185e15277f68fed0104ef80be
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-19 21:24:05 -0300

    feat: add driver script

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] Color32 isDeliveringColor = Color.white;
+
+    SpriteRenderer spriteRenderer;
+    bool hasPackage = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Package" && !hasPackage)
+        {
+            hasPackage = true;
+            spriteRenderer.color = isDeliveringColor;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.tag == "Customer" && hasPackage)
+        {
+            hasPackage = false;
+            spriteRenderer.color = Color.white;
+        }
     }
 }
