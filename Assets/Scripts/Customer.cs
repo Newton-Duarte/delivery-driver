221dd43ca1b2e14b2df8fd1f50badc8c1320e13f
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-21 20:43:28 -0300

    feat: add customer script

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    [SerializeField] public int id;
+    [SerializeField] public GameObject package;
+}
